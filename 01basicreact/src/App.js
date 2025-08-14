e712c1846256167f6204621e36daef5689c5a957
aguptashyam <shyam.anubhav10@gmail.com>
2025-08-14 19:12:11 +0530

    Initialize React project with Basic React and Vite setup

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>React JS | in this I learned how to create react project using create-react-app</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>React JS with Vite | in this I learned to create react projects using vite</h1>
+  )
+}
+
+export default App
